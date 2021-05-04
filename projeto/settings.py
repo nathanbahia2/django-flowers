7efde201e45654338590f92867ce2d7202f2e8fa
nathanbahia2 <nathan@datatype.com.br>
2021-05-04 11:23:21 -0300

    Ajusta banco de dados postgree

--- projeto/settings.py
+++ projeto/settings.py
@@ -86,16 +86,8 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = dict()
-    DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'))
+DATABASES = dict()
+DATABASES['default'] = dj_database_url.parse(config('DATABASE_URL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
