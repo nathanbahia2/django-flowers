9e8928a2c92294441f71cad9cde84afda9574e14
nathanbahia2 <nathan@datatype.com.br>
2021-05-04 17:41:22 -0300

    Ajusta secret_key

--- projeto/settings.py
+++ projeto/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '-182mt)ud+91noau_k@=#-*3ns0p83nw3$g)6xxjepr&sng7bz='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
