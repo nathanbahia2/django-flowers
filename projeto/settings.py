d0c2512240fe03618b88ebdc79b997c31422c139
nathanbahia2 <nathan@datatype.com.br>
2021-05-07 09:50:19 -0300

    ajustes para decouple

--- projeto/settings.py
+++ projeto/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_siwg0)64a=c3j-l*v=b=7djspis1kr_$u!pmrbx(5vu99p0p#'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=True)
